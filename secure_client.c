323aa92aa1603b74e15ebbe3b055819ad6b85be0
MatthewChatten <MatthewChatten@users.noreply.github.com>
2015-05-05 15:56:37 -0400

    multithreading

--- secure_client.c
+++ secure_client.c
@@ -20,6 +20,14 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+
+void* readmess(void* p);
+
+struct threaddata {
+	int cfd; //for file descriptor
+	struct sockaddr_in servad; //for address of server
+}; 
+
 int main(int argc, char **argv)
 {
 	int fd;	
@@ -32,11 +40,11 @@ int main(int argc, char **argv)
 
 	struct sockaddr_in clientaddr; //built structure for client socket
 	bzero((char *) &clientaddr, sizeof(clientaddr));
-    	clientaddr.sin_family = AF_INET; 
-    	clientaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+	clientaddr.sin_family = AF_INET; 
+	clientaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
 	clientaddr.sin_port = htons(0); //random port
  
-    	if(bind(fd, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0) //bind socket
+	if(bind(fd, (struct sockaddr *)&clientaddr, sizeof(clientaddr)) < 0) //bind socket
 	{
 		perror("Bind failed.");
 		return 0;
@@ -44,23 +52,66 @@ int main(int argc, char **argv)
 
 	struct sockaddr_in serveraddr;
 	bzero((char *) &serveraddr, sizeof(serveraddr));
-    	serveraddr.sin_family = AF_INET; 
+	serveraddr.sin_family = AF_INET; 
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    	serveraddr.sin_port = htons(8080);
+	serveraddr.sin_port = htons(8080);
 
-	printf("Enter a message to start.\n");
-	char test[512];
-	if(fgets(test, sizeof(test), stdin) == NULL)
+	struct threaddata args;
+	args.cfd = fd; //save file descriptor
+	args.servad = serveraddr; //save server address
+
+	pthread_t tid;
+	int rc;
+	if((rc = pthread_create(&tid, NULL, readmess, &args)) != 0)
 	{
-		perror("Invalid message.");
+		perror("Failed to create thread.");
+		return 0;
+	}
+	if((rc = pthread_detach(tid)) != 0) //don't need to rejoin later
+	{
+		perror("Failed to detach thread.");
 		return 0;
 	}
 
-	if(sendto(fd, test, strlen(test), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
-	{ 
-		perror("The sendto command failed."); 
-		return 0; 
+	while(1)
+	{
+		printf("Enter a message.\n");
+		char test[512];
+		if(fgets(test, sizeof(test), stdin) == NULL)
+		{
+			perror("Invalid message.");
+			return 0;
+		}
+
+		if(sendto(fd, test, strlen(test), 0, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
+		{ 
+			perror("The sendto command failed."); 
+			return 0; 
+		}
 	}
 
 	return 0;
+}
+
+/*
+ * readmess - Handles all reads from server.
+ * 
+ * The inputs are the fd of the client socket and the serveraddr struct.
+ */
+void* readmess(void* parameters) 
+{
+	struct threaddata* p = (struct threaddata*) parameters; //get pointer to struct
+	int sfd = p->cfd; //save values from struct to local variables
+	struct sockaddr_in saddr = p->servad;
+
+	printf("Entering thread. sfd: %d\n", sfd);
+
+	/*int rc;
+	if((rc = close(sfd)) < 0)
+	{
+		perror("Failed to close socket.");
+		return 0;
+	}*/
+
+	return NULL;
 }
